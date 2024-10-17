a600c514bb75ab00d7ca7333f721719637a702e2
dengbo <ydyjy123@gmail.com>
2024-10-17 09:00:35 +0800

    modify font load

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,12 +1,15 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
-local font = 'JetBrainsMono Nerd Font'
+-- local font = 'MesloLGS NF'
 local font_size = platform().is_mac and 12 or 12
 
 return {
-   font = wezterm.font(font),
    font_size = font_size,
+   font = wezterm.font_with_fallback({
+      'MesloLGS NF',
+      'JetBrainsMono Nerd Font',
+   }),
 
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
    freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
